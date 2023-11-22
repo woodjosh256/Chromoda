ed3e0910e24dda77910d504380c72c5ed2759fa2
joshwood <wood.josh256@gmail.com>
2023-11-22 10:41:43 -0600

    Updated and fix url responses.

--- backend/lambdas/new_order_random.py
+++ backend/lambdas/new_order_random.py
@@ -1,19 +1,17 @@
+import random
 import string
-from random import random
 
 try:
     import unzip_requirements
 except ImportError:
     pass
 
-from _decimal import Decimal
-
 from botocore.exceptions import ClientError
 
 from typing import Any, Dict
 import boto3
 
-from .common.api_responses import _200, _400, _500
+from .common.api_responses import _200, _500
 
 def handler(event: Dict[str, Any], context: Any) -> Dict:
     dynamodb = boto3.resource('dynamodb')
@@ -34,13 +32,6 @@ def handler(event: Dict[str, Any], context: Any) -> Dict:
 
     return _200({
         "print_id": print_id,
-        "url": f"https://acromodapacks.com/{print_id}"
+        "url": f"https://www.acromodapacks.com/{print_id}",
+        "internal": f"https://drybag-internal-tool.d1065fsy0rl8xl.amplifyapp.com/{print_id}"
     })
-
-# GET https://vj00e2kyw2.execute-api.us-east-1.amazonaws.com/dev/generateTopo?
-#     tl_lon=-94.9213&tl_lat=49.4823&tr_lon=-73.7544&tr_lat=49.4822&bl_lon=-94.9214&bl_lat=39.4275&br_lon=-73.7545&
-#     br_lat=39.4274&lines=25&lines=25&width=1000
-
-# the url for this lambda is: https://8sbys0hxkb.execute-api.us-east-1.amazonaws.com/dev/saveOrder
-# this is a url with query parameters filled out for testing purposes:
-# https://8sbys0hxkb.execute-api.us-east-1.amazonaws.com/dev/saveOrder?print_id=123&tl_lon=-94.9213&tl_lat=49.4823&tr_lon=-73.7544&tr_lat=49.4822&bl_lon=-94.9214&bl_lat=39.4275&br_lon=-73.7545&br_lat=39.4274&color_a=FFFFFF&color_b=FF0000&gradient=true&secondary=true&text=hello&coordinates=true
